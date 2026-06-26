8e275ea034f7aaecbfb471b3ea04cd311d00f8e2
Rishikesh <rishikeshjain64@gmail.com>
2026-06-27 02:59:07 +0530

    1-Currently added some dependency in  pom.xml , which are directly related to Jwt token and services. 2- Jwt Configuration added in application.properties. 3- AuthResponse created in DTO successfully. 4- JwtService has been created in Security package , implementation is left out.

---
+++ src/main/java/com/Demo/JwtToken/dto/AuthResponse.java
@@ -0,0 +1,16 @@
+package com.Demo.JwtToken.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class AuthResponse {
+
+    private String accessToken;
+
+    private String refreshToken;
+
+    private String message;
+
+}
